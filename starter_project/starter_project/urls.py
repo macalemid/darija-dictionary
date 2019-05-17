6d337c0fa11acd8371a9d227efa70bce7754be79
mikahmed <mikahmed@iu.edu>
2019-05-17 11:05:18 -0400

    added dictionary page

--- starter_project/starter_project/urls.py
+++ starter_project/starter_project/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from dictionaryy import views as dict_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dictionary/', dict_views.home, name='dictionary'),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
