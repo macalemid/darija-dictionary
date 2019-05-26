39246a2c9c2b2b284a9a3299337a5a3aca53ea7a
mikahmed <mikahmed@iu.edu>
2019-05-26 08:18:39 -0400

    created update functionality for words

--- starter_project/dictionaryy/urls.py
+++ starter_project/dictionaryy/urls.py
@@ -1,10 +1,12 @@
-
-from . import views
 from django.urls import path
+from . import views
+
 
 
 urlpatterns = [
 path('', views.home, name='dictionary'),
 path('word/<int:pk>/', views.WordDetailView.as_view(), name='word-detail'),
 path('word/new/', views.WordCreateView.as_view(), name='word-create'),
+path('word/<int:pk>/update', views.WordUpdateView.as_view(), name='word-update'),
+
     ]
