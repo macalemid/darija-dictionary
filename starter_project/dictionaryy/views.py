55cfd0c9c8fe7d8cd775c19dfacd7818cbe020e5
mikahmed <mikahmed@iu.edu>
2019-05-29 15:06:18 -0400

    changed the structure of dictionary. Added WordD and Dictinoary and removed Word

--- starter_project/dictionaryy/views.py
+++ starter_project/dictionaryy/views.py
@@ -4,36 +4,16 @@ from django.db.models import Q
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
+from dictionaryy.models import Dictionary, WordD
 def home(request):
-	words = Word.objects.all()
-
+	dicts = Dictionary.objects.all()
+	"""
 	if('search' in request.GET):
 		search_term = request.GET['search']
 		words = Word.objects.filter(darija__contains=search_term)
-	
+	"""
 	context = {
-		'words': words
+		'dicts': dicts
 	}
 	return render(request, 'dictionaryy/home.html', context)
 
-
-class WordDetailView(DetailView):
-	model = Word
-
-class WordCreateView(LoginRequiredMixin, CreateView):
-	model = Word
-	fields = ['darija', 'arabic', 'english', 'explanation']
-
-	def form_valid(self, form):
-		form.instance.author = self.request.user
-		return super().form_valid(form)
-
-class WordUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-	model = Word
-	fields = ['darija', 'arabic', 'english', 'explanation']		
-
-	def test_func(self):
-		word = self.get_object()
-		if self.request.user == word.author:
-			return True
-		return False
