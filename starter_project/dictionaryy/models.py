5ca2fc47fab5c551c2f930d80a35b61513334349
mikahmed <mikahmed@iu.edu>
2019-05-29 15:06:18 -0400

    changed the structure of dictionary. Added WordD and Dictinoary and removed Word

--- starter_project/dictionaryy/models.py
+++ starter_project/dictionaryy/models.py
@@ -4,19 +4,7 @@ from django.urls import reverse
 
 
 class Word(models.Model):
-	darija = models.CharField(max_length=100)
-	arabic = models.CharField(max_length=100)
-	english = models.CharField(max_length=100)
-	explanation = models.TextField()
-	picture = False
-	audio = False
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-	def __str__(self):
-		return self.darija
-	
-	def get_absolute_url(self):
-		return reverse('word-detail', kwargs={'pk': self.pk})
+	pass
 
 class WordD(models.Model):
 	LANGUAGES = [
@@ -24,15 +12,39 @@ class WordD(models.Model):
 		('A', 'Arabic'),
 		('E', 'English'),
 		]
-	Language = models.CharField(max_length=1, choices=LANGUAGES)
+	word = models.CharField(max_length=201, null=True)
+	language = models.CharField(max_length=1, choices=LANGUAGES)
 	plural = models.CharField(max_length=20)
 	root = models.CharField(max_length=10)
 	audio = models.FileField(upload_to='audio/')
+
+	def __str__(self):
+		return self.word
 
 class Dictionary(models.Model):
 	words = models.ManyToManyField(WordD)
 	explanation = models.CharField(max_length=10000)
 	picture = models.ImageField(upload_to='images/')
 	tags = []
+	user = models.ForeignKey(
+		User,
+		null=True,
+		on_delete=models.SET_NULL
+		)
+
+	def get_darija(self):
+		return self.words.filter(language='D').first()
+
+	def get_arabic(self):
+		return self.words.filter(language='A').first()
+
+	def get_english(self):
+		return self.words.filter(language='E').first()
+
+	def __str__(self):
+		return self.explanation
+	
+	def get_absolute_url(self):
+		return reverse('word-detail', kwargs={'pk': self.pk})
 
 	
