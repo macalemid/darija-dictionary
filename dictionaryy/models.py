879cbb7f3e6816f6211a726d3ebc3d73b1dd2380
mikal <mikalyusufahmed@gmail.com>
2023-10-28 19:40:05 -0500

    Added validation for characters

--- dictionaryy/models.py
+++ dictionaryy/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 from django.urls import reverse
 
 class Dictionary(models.Model):
-	darija = models.CharField(max_length=20, blank=True)
-	arabic = models.CharField(max_length=20, blank=True)
-	english = models.CharField(max_length=20, blank=True)
+	darija = models.CharField(
+		max_length=20,
+		blank=True,
+		validators = [
+			RegexValidator('^[\u0621-\u064A\u0660-\u0669a-zA-Z0-9 ]+$', "Darija can only contain Latin/Arabic letters and numerals.", code="darija-invalid")
+    ]
+  )
+	arabic = models.CharField(
+		max_length=20,
+		blank=True,
+		validators = [
+		  RegexValidator('^[\u0621-\u064A\u0660-\u0669 ]+$', "Arabic can only contain Arabic letters and numerals.", code="arabic-invalid")
+    ]
+  )
+	english = models.CharField(
+		max_length=20,
+		blank=True,
+		validators = [
+			RegexValidator('^[a-zA-Z0-9 ]+$', "English can only contain Latin letters and numerals.", code="darija-invalid")
+      ]
+		)
 	user = models.ForeignKey(
 		User,
 		null=True,
@@ -15,4 +34,3 @@ class Dictionary(models.Model):
 	
 	def get_absolute_url(self):
 		return reverse('dictionary-detail', kwargs={'pk': self.pk})
-
